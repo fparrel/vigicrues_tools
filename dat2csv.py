b1e6a0aabf9f4ff293ae4f930cf843df6e7e87d8
Frederic fparrel <fredericparrel@gmail.com>
2017-03-17 17:11:19 +0100

    Added last charts

--- dat2csv.py
+++ dat2csv.py
@@ -2,29 +2,41 @@
 
 import struct
 import json
+import datetime
 
 serialisation='<Lf'
 value_size = struct.calcsize(serialisation)
 
-def read(station_id):
-    f=open('%s.dat'%station_id,'rb')
-    while f:
-        buf=f.read(value_size)
+def dat2csv(station_id,min_date,nb_min=15):
+    fdat=open('%s.dat'%station_id,'rb')
+    fcsvfull=open('viewer/%s.csv'%station_id,'w')
+    fcsvfull.write('datetime,value\n')
+    fcsvlast=open('viewer/%s-last.csv'%station_id,'w')
+    fcsvlast.write('datetime,value\n')
+    nb_on_last = 0
+    last_lines = []
+    while True:
+        buf=fdat.read(value_size)
         if len(buf)>0:
-            yield struct.unpack(serialisation,buf)
+            timestamp,value = struct.unpack(serialisation,buf)
+            csvline = '%s,%f\n'%(timestamp,value)
+            fcsvfull.write(csvline)
+            last_lines.append(csvline)
+            last_lines=last_lines[:nb_min]
+            if datetime.date.fromtimestamp(timestamp)>=min_date:
+                fcsvlast.write(csvline)
+                nb_on_last += 1
         else:
             break
-    f.close()
-
-def dat2csv(station_id):
-    f=open('viewer/%s.csv'%station_id,'w')
-    f.write('datetime,value\n')
-    f.write('\n'.join(map(lambda data: '%s,%f'%(data[0],data[1]),read(station_id))))
-    f.close()
+    if nb_on_last<nb_min:
+        fcsvlast.write(''.join(last_lines))
+    fcsvfull.close()
+    fcsvlast.close()
+    fdat.close()
 
 if __name__=='__main__':
     f=open('viewer/stations.json','r')
     stations = json.load(f)
     f.close()
     for station in stations:
-        dat2csv(station['id'])
+        dat2csv(station['id'],datetime.date.today())
