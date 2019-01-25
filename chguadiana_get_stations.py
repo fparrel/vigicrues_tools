143c4a508bcac7ddd54be9be001c16fe5909b7be
Frederic PARREL <fredericparrel@gmail.com>
2019-01-25 22:57:49 +0100

    Extract river from station name on chguadiana

--- chguadiana_get_stations.py
+++ chguadiana_get_stations.py
@@ -7,6 +7,8 @@ import json
 
 pat = re.compile(r'./charts/charts_tr_([a-z]+).php\?station=([A-Z0-9-]+).*\&label=([^\&]+)')
 
+river_at = re.compile('(.*)(\sen\s|a\.\sab\.|a\.\sarr\.)(.*)')
+
 prefix = 'http://www.saihguadiana.com:7080/visorTR/'
 
 r = requests.get(prefix + 'index.php')
@@ -18,7 +20,13 @@ def parseStation(e):
     href = e.get('href')
     t,station_id,station_name = pat.findall(href)[0]
     url = prefix + href
-    return {'type':t,'id':station_id,'name':station_name,'url':url}
+    m = river_at.findall(station_name)
+    if len(m)==1:
+        river = m[0][0]
+    else:
+        river = station_name
+    print m,river
+    return {'type':t,'id':station_id,'name':station_name,'url':url,'river':river}
 
 links = map(parseStation,tree.xpath('//td/a[starts-with(@href,"./charts/charts_tr_")]'))
 
