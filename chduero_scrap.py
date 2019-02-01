6e360d6793a400cd685c5a068dbfcfaaa58d4203
Frederic PARREL <fredericparrel@gmail.com>
2019-02-02 00:08:20 +0100

    Remove useless prefix in duero and tajo

--- chduero_scrap.py
+++ chduero_scrap.py
@@ -14,11 +14,11 @@ def parsePoint(p):
             v = float(s[1])
         return datetime.datetime.strptime(s[0][4:-3],"%d/%m/%Y %H:%M"),v
     except:
-        print 'Could not parse:',p
+        print 'Could not parse:', p
     return None
 
 def get_data(station_id):
-    r = requests.get('http://www.saihduero.es/ficha-risr?r=%s'%station_id)
+    r = requests.get('http://www.saihduero.es/ficha-risr?r=%s' % station_id)
     t = r.text.encode(r.encoding)
     i = t.find('<td>Caudal</td>')
     i = t.find('href=',i)
@@ -30,13 +30,13 @@ def get_data(station_id):
     i = t.find('{',i)
     j = t.find('];',i)
     d = t[i:j]
-    return filter(lambda x:x!=None,map(parsePoint,d.split('},\n'))),direct_url
+    return filter(lambda x: x!=None, map(parsePoint, d.split('},\n'))), direct_url
 
 def process(station_id):
-    values_gen,direct_url = get_data(station_id)
+    values_gen, direct_url = get_data(station_id)
     values = list(values_gen)
-    if len(values)>0:
-        save_values('chduero','debit_%s'%station_id,values)
+    if len(values) > 0:
+        save_values('chduero', station_id, values)
     return direct_url
 
 def main():
