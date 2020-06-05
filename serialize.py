8f796bade5feb220a015db71daf9cb6d4014973b
fparrel <info@regepe.com>
2020-06-05 15:51:14 +0200

    Add LZ4 compression

--- serialize.py
+++ serialize.py
@@ -4,6 +4,7 @@ import datetime
 import os
 import time
 import json
+import lz4.frame
 
 def mkdirp(path):
     try:
@@ -19,7 +20,7 @@ def getLastUpdateTime(domain,station_id):
 
     # Open the file for appending, create new file is needed. 'b' (binary) flag is needed on windows
     try:
-        f = open('data/%s/%s.dat' % (domain, station_id), 'rb')
+        f = lz4.frame.open('data/%s/%s.dat.lz4' % (domain, station_id), 'rb')
     except:
         return None
         #return datetime.datetime.fromtimestamp(0)
@@ -43,34 +44,41 @@ def saveValues(domain,station_id,values):
 
     # Open the file for appending, create new file is needed. 'b' (binary) flag is needed on windows
     mkdirp('data/%s' % (domain))
-    f = open('data/%s/%s.dat' % (domain, station_id), 'a+b')
 
-    # Seek to the end (it seems that a+b opening do not seek to the end)
-    f.seek(0,os.SEEK_END)
-
-    # Find the values already in the file
+    fname = 'data/%s/%s.dat.lz4' % (domain, station_id)
     minidx = 0
-    if f.tell() > value_size:
-        # Read and parse last value
-        f.seek(-value_size, os.SEEK_END)
-        last_value = struct.unpack(serialisation,f.read(value_size))
-        last_datetime = datetime.datetime.fromtimestamp(last_value[0])
-        print('last from .dat: %s, from http server: %s - %s' % (last_datetime, values[0][0], values[-1][0]))
-        # Discard values from scraping that are older than last value from file
-        for dt, v in values:
-            if last_datetime < dt:
-                break
-            minidx += 1
+    if os.path.exists(fname):
+    	f = lz4.frame.open(fname, 'rb')
 
-    # Seek back to the end: even if we did seek(-value_size) and read(value_size), seeking is mandatory on windows bettween read and write
-    f.seek(0,os.SEEK_END)
+        # Seek to the end (it seems that a+b opening do not seek to the end)
+        f.seek(0,os.SEEK_END)
+
+        # Find the values already in the file
+        if f.tell() > value_size:
+            # Read and parse last value
+            f.seek(-value_size, os.SEEK_END)
+            last_value = struct.unpack(serialisation,f.read(value_size))
+            last_datetime = datetime.datetime.fromtimestamp(last_value[0])
+            print('last from .dat: %s, from http server: %s - %s' % (last_datetime, values[0][0], values[-1][0]))
+            # Discard values from scraping that are older than last value from file
+            for dt, v in values:
+                if last_datetime < dt:
+                    break
+                minidx += 1
+
+        # Seek back to the end: even if we did seek(-value_size) and read(value_size), seeking is mandatory on windows bettween read and write
+        #f.seek(0,os.SEEK_END)
+        f.close()
+
     # Append data to the file
     buf = ''.join(map(lambda v: struct.pack(serialisation,time.mktime(v[0].timetuple()),v[1]),list(values)[minidx:]))
+
+    f = lz4.frame.open('data/%s/%s.dat.lz4' % (domain, station_id), 'ab')
     f.write(buf)
     f.close()
 
 def checkData(domain,station_id):
-    f = open('data/%s/%s.dat' % (domain, station_id), 'r')
+    f = lz4.frame.open('data/%s/%s.dat.lz4' % (domain, station_id), 'r')
     # check that we have t in growing order
     t_prev = 0
     min_v = None
@@ -93,7 +101,7 @@ def checkData(domain,station_id):
     return True, min_v, max_v
 
 def comptePointsMatching(domain,station_id,match_func,fullyearonly=False):
-    f = open('data/%s/%s.dat' % (domain, station_id), 'r')
+    f = lz4.frame.open('data/%s/%s.dat.lz4' % (domain, station_id), 'r')
     if fullyearonly:
         f.seek(-value_size,os.SEEK_END)
         t_last, v = struct.unpack(serialisation,f.read(value_size))
@@ -120,8 +128,8 @@ def comptePointsMatching(domain,station_id,match_func,fullyearonly=False):
     return cptr,t-t_first
 
 def repairData(domain,station_id):
-    fname = 'data/%s/%s.dat' % (domain, station_id)
-    f = open(fname, 'r')
+    fname = 'data/%s/%s.dat.lz4' % (domain, station_id)
+    f = lz4.frame.open(fname, 'r')
     values = []
     while True:
         r = f.read(value_size)
@@ -134,7 +142,7 @@ def repairData(domain,station_id):
         values = filter(lambda tv:tv[1]!=-9999.0, values) # Filter undef
         print len(values)
     values.sort(key=lambda tv:tv[0])
-    f = open(fname, 'w')
+    f = lz4.frame.open(fname, 'w')
     f.write(''.join([struct.pack(serialisation,tv[0],tv[1]) for tv in values]))
     f.close()
 
